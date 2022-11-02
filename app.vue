db732df2b36d258abaab2ec8eefbd41d376f5913
Kasope Johnson <kasopej@gmail.com>
2022-11-03 00:47:06 +0100

    adds base scss styling

--- app.vue
+++ app.vue
@@ -6,14 +6,8 @@
   </div>
 </template>
 
-<script setup>
-  // let swapCountHelper = 0;
-  // const currentComponent = shallowRef(resolveComponent("LazyCounterInput"));
-  // function swapComponents() {
-  //   swapCountHelper++;
-  //   currentComponent.value =
-  //     swapCountHelper % 2 === 0
-  //       ? resolveComponent("LazyCounterInput")
-  //       : resolveComponent("LazyCounterHeading");
-  // }
-</script>
+<script setup></script>
+
+<style lang="scss">
+  @import "~/assets/scss/base.scss";
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
-    <nav class="flex bg-purple-700">
-      <ul class="main-nav nav-list ml-3">
-        <li class="inline p-0">
-          <nuxt-link to="/">Home</nuxt-link>
-          <nuxt-link to="/">Iphones</nuxt-link>
-          <nuxt-link to="/">Cart()</nuxt-link>
-          <nuxt-link to="/">Total Route Changes: 1</nuxt-link>
+    <nav class="flex bg-purple-800">
+      <ul class="main-nav nav-list ml-2 p-3">
+        <li class="inline px-2">
+          <nuxt-link to="/" class="text-lg">Home</nuxt-link>
+        </li>
+        <li class="inline px-2">
+          <nuxt-link to="/" class="text-lg">Iphones</nuxt-link>
+        </li>
+        <li class="inline px-2">
+          <nuxt-link to="/" class="text-lg">Cart()</nuxt-link>
+        </li>
+        <li class="inline px-2">
+          <nuxt-link to="/" class="text-lg">Total Route Changes: 1</nuxt-link>
         </li>
       </ul>
-      <ul class="auth-nav nav-list ml-auto mr-3">
-        <li class="inline p-0">
-          <nuxt-link to="/">Login</nuxt-link>
+      <ul class="auth-nav nav-list ml-auto mr-2 p-3">
+        <li class="inline px-2">
+          <nuxt-link to="/" class="text-lg">Login</nuxt-link>
         </li>
       </ul>
     </nav>
