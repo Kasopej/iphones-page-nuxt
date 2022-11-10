b3c2c8559e80412c90ad0ff3ab437e2e49d8f144
Kasope Johnson <kasopej@gmail.com>
2022-11-10 09:49:25 +0100

    sends login request to reqres

--- pages/login.vue
+++ pages/login.vue
@@ -6,13 +6,18 @@
         alt="woman holding key - login illustration"
       />
     </div>
-    <form class="col-50 pr-6 pl-14 flex flex-col justify-center">
+    <form
+      @submit.prevent="callLogin"
+      class="col-50 pr-6 pl-14 flex flex-col justify-center"
+    >
       <input
+        v-model="loginEntry.email"
         class="w-full rounded-sm border-slate-400 mb-8 form-input p-3"
         type="text"
         placeholder="email"
       />
       <input
+        v-model="loginEntry.password"
         class="w-full rounded-sm border-slate-400 mb-8 form-input p-3"
         type="password"
         placeholder="password"
@@ -27,6 +32,26 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+  import { LOGIN_URL } from "@/http/routes";
+  useHead({
+    title: "App Login",
+  });
+  const loginEntry = reactive({
+    email: "eve.holt@reqres.in",
+    password: "cityslicka",
+  });
+
+  function callLogin() {
+    if (loginEntry.email && loginEntry.password) {
+      useFetch(LOGIN_URL, {
+        method: "post",
+        body: loginEntry,
+      }).then((res) => {
+        console.log(res.data.value);
+      });
+    }
+  }
+</script>
 
 <style lang="scss" scoped></style>
